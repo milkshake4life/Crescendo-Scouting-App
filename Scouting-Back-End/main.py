9f1be08f67ab3050f02871c2d6a5345c6ca76d8e
milkshake4life <elee91214@gmail.com>
2024-02-03 12:51:30 -0800

    added list of teamNumbers

--- Scouting-Back-End/main.py
+++ Scouting-Back-End/main.py
@@ -1,5 +1,19 @@
 import requests
+from firebase import firebase
+import json
 
-headers = {'X-TBA-Auth-Key': ''}
-r = requests.get('https://www.thebluealliance.com/api/v3/status', headers=headers)
-print(r)
+f = open("../key.txt", "r")
+eventKey = open("../eventKey.txt", "w")
+headers = {'X-TBA-Auth-Key': f.read()}
+r = requests.get('https://www.thebluealliance.com/api/v3/event/2023cave/teams', headers=headers)
+x = json.loads(r.text)
+teamNumList = []
+for i in range(0, len(x)):
+    teamNumList.append(x[i]["team_number"])
+teamNumList.sort()
+for i in range(0, len(x)):
+    eventKey.write(str(teamNumList[i]) + " - ")
+
+
+
+firebase = firebase.FirebaseApplication('https://crescendo-scouting-app-default-rtdb.firebaseio.com/', None)
