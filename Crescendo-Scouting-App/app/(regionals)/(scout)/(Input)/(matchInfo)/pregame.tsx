dc53f527a294bc16b2f3067eccee62252e91219d
milkshake4life <elee91214@gmail.com>
2024-02-17 12:54:48 -0800

    basic Pre Game Info Page

--- Crescendo-Scouting-App/app/(regionals)/(scout)/(Input)/(matchInfo)/pregame.tsx
+++ Crescendo-Scouting-App/app/(regionals)/(scout)/(Input)/(matchInfo)/pregame.tsx
@@ -1,25 +1,145 @@
-import { Link, router } from "expo-router";
-import { Pressable, Button, Text, View, StyleSheet } from "react-native";
+import { Link, router, useGlobalSearchParams } from "expo-router";
+import { Pressable, Button, Text, View, StyleSheet, ScrollView } from "react-native";
 import BackButton from "../../../../backButton";
+import { onValue, ref } from "@firebase/database";
+import { database } from "../../../../../firebaseConfig";
+import { useEffect, useState } from "react";
+import { Dropdown } from "react-native-element-dropdown";
+import { CheckBox } from "react-native-elements";
+import React from "react";
 
 
 const matchInfo = () => {
+
+  const { regional } = useGlobalSearchParams<{ regional: string }>();
+  const { teamNumber } = useGlobalSearchParams<{ teamNumber: string }>();
+  let modifiedRegional = regional
+  if (regional === 'Orange County') {
+    modifiedRegional = 'Orange-County'
+  }
+
+  interface DropdownItem {
+    label: string;
+    value: string;
+  }
+
+  const [dropdownData, setDropdownData] = useState<DropdownItem[]>([]);
+  const [isFocus, setIsFocus] = useState(false);
+  const [selectedValue, setSelectedValue] = useState<string | null>(null);
+  const [selectedAlliance, setSelectedAlliance] = useState<"Red" | "Blue" | null>(null);
+  const [selectedSeating, setSelectedSeating] = useState<"Amp" | "Source" | null>(null);
+  const [selectedStartingPosition, setSelectedStartingPosition] = useState<"Amp" | "Middle" | "Source" | null>(null);
+
+  const fetchTeams = () => {
+    const qualMatchRef = ref(database, modifiedRegional + '/teams/' + teamNumber + '/Match-Info'); // Adjusted path
+    onValue(qualMatchRef, (snapshot) => {
+      const qualMatch = snapshot.val();
+      if (qualMatch) {
+        const processedData = Object.keys(qualMatch).map((qualMatch) => {
+          return {
+            label: qualMatch,  // Using the team number as the label
+            value: qualMatch,  // Also using the team number as the value
+          };
+        });
+        setDropdownData(processedData);
+      } else {
+        setDropdownData([]);
+      }
+    });
+  };
+
+  useEffect(() => {
+    fetchTeams();
+  }, []);
+
   return (
-    <View>
-      <BackButton buttonName="Home Page" />
-      <Text>Plese Input the Pre Game Information!</Text>
-      <Pressable
-            style={styles.buttonOne}
-            onPress={() => router.push(`/(matchInfo)/auto`)}
-            >
-                <Text style={styles.buttonOneText}>Auto</Text>
-            </Pressable>
-    </View>
+    <ScrollView>
+      <View>
+        <BackButton buttonName="Home Page" />
+        <Text>Plese Input the Pre Game Information!</Text>
+        <Text>Qualification Match</Text>
+        <Dropdown
+          style={[styles.dropdown, isFocus && { borderColor: 'blue' }]}
+          placeholderStyle={styles.placeholderStyle}
+          selectedTextStyle={styles.selectedTextStyle}
+          inputSearchStyle={styles.inputSearchStyle}
+          iconStyle={styles.iconStyle}
+          data={dropdownData}
+          search
+          maxHeight={300}
+          labelField="label"
+          valueField="value"
+          placeholder={!isFocus ? 'Select item' : '...'}
+          searchPlaceholder="Search..."
+          onFocus={() => setIsFocus(true)}
+          onBlur={() => setIsFocus(false)}
+          onChange={item => {
+            setSelectedValue(item.value);  // Update the state to the new value
+            setIsFocus(false);  // Assuming you want to unfocus the dropdown after selection
+          }}
+        />
+        <Text>Alliance</Text>
+        <CheckBox
+          title="Red"
+          checked={selectedAlliance === "Red"}
+          onPress={() => setSelectedAlliance(selectedAlliance === "Red" ? null : "Red")}
+          containerStyle={styles.checkboxContainer}
+        />
+        <CheckBox
+          title="Blue"
+          checked={selectedAlliance === "Blue"}
+          onPress={() => setSelectedAlliance(selectedAlliance === "Blue" ? null : "Blue")}
+          containerStyle={styles.checkboxContainer}
+        />
+
+        <Text>Seating</Text>
+        <CheckBox
+          title="Amp"
+          checked={selectedSeating === "Amp"}
+          onPress={() => setSelectedSeating(selectedSeating === "Amp" ? null : "Amp")}
+          containerStyle={styles.checkboxContainer}
+        />
+        <CheckBox
+          title="Source"
+          checked={selectedSeating === "Source"}
+          onPress={() => setSelectedSeating(selectedSeating === "Source" ? null : "Source")}
+          containerStyle={styles.checkboxContainer}
+        />
+
+        <Text>Starting Position</Text>
+        <CheckBox
+          title="Amp"
+          checked={selectedStartingPosition === "Amp"}
+          onPress={() => setSelectedStartingPosition(selectedStartingPosition === "Amp" ? null : "Amp")}
+          containerStyle={styles.checkboxContainer}
+        />
+        <CheckBox
+          title="Middle"
+          checked={selectedStartingPosition === "Middle"}
+          onPress={() => setSelectedStartingPosition(selectedStartingPosition === "Middle" ? null : "Middle")}
+          containerStyle={styles.checkboxContainer}
+        />
+        <CheckBox
+          title="Source"
+          checked={selectedStartingPosition === "Source"}
+          onPress={() => setSelectedStartingPosition(selectedStartingPosition === "Source" ? null : "Source")}
+          containerStyle={styles.checkboxContainer}
+        />
+
+        <Pressable
+          style={styles.buttonOne}
+          onPress={() => router.push(`/(matchInfo)/auto`)}
+        >
+          <Text style={styles.buttonOneText}>Auto</Text>
+        </Pressable>
+      </View>
+    </ScrollView>
+
   );
 };
 
 const styles = StyleSheet.create({
-  backButtonText:{
+  backButtonText: {
     fontFamily: 'BPoppins',
     fontSize: 15,
     color: 'white',
@@ -39,6 +159,7 @@ const styles = StyleSheet.create({
   },
   buttonOne: {
     marginTop: 0,
+    marginBottom: 20,
     alignItems: 'center',
     justifyContent: 'center',
     paddingVertical: 12,
@@ -56,6 +177,43 @@ const styles = StyleSheet.create({
     letterSpacing: 0.25,
     color: 'white',
     fontFamily: 'BPoppins',
+  },
+  dropdown: {
+    height: 50,
+    width: '80%', // or some other appropriate width
+    borderColor: 'gray',
+    borderWidth: 0.5,
+    borderRadius: 8,
+    paddingHorizontal: 8,
+    // Add margin for some spacing if needed
+    marginTop: 10,
+    marginBottom: 40,
+  },
+  label: {
+    position: 'absolute',
+    backgroundColor: 'white',
+    left: 22,
+    top: 8,
+    zIndex: 999,
+    paddingHorizontal: 8,
+    fontSize: 14,
+  },
+  placeholderStyle: {
+    fontSize: 16,
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
+  },
+  checkboxContainer: {
+    marginVertical: 20,
   },
 });
 
